4bafce82d66f280902ab09e3bb218c2a2a5a2dc3
v.saltanova <vikasaltanova@mail.ru>
2018-01-29 17:38:34 +0700

    added splash activity

--- app/src/main/java/noname/shift/getmoney/SplashActivity.java
+++ app/src/main/java/noname/shift/getmoney/SplashActivity.java
@@ -1,5 +1,6 @@
 package noname.shift.getmoney;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,6 +9,15 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
