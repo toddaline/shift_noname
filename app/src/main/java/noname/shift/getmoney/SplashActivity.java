5c2459a0ef3f161884548bfe281d32f3fe946976
v.saltanova <vikasaltanova@mail.ru>
2018-02-02 17:48:02 +0700

    fixed splash

--- app/src/main/java/noname/shift/getmoney/SplashActivity.java
+++ app/src/main/java/noname/shift/getmoney/SplashActivity.java
@@ -1,9 +1,11 @@
 package noname.shift.getmoney;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -13,24 +15,37 @@ public class SplashActivity extends AppCompatActivity {
 
     private static final int DELAY = 2000;
 
+    @SuppressLint("HandlerLeak")
+    private Handler handler = new Handler() {
+
+        @Override
+        public void handleMessage(Message msg) {
+            if (msg.what == 0) {
+                SharedPreferences settings = getSharedPreferences(SharedPreferencesConstants.APP_PREFERENCES, Context.MODE_PRIVATE);
+                boolean hasTable = settings.getBoolean(SharedPreferencesConstants.APP_PREFERENCES_HAS_TABLE, false);
+                if (hasTable) {
+                    Intent intent = new Intent(SplashActivity.this, ListActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        handler.sendEmptyMessageDelayed(0, DELAY);
+    }
 
-        SharedPreferences settings = getSharedPreferences(SharedPreferencesConstants.APP_PREFERENCES, Context.MODE_PRIVATE);
-        boolean hasTable = settings.getBoolean(SharedPreferencesConstants.APP_PREFERENCES_HAS_TABLE, false);
-
-        new Handler().postDelayed(() -> {
-            if (hasTable) {
-                Intent intent = new Intent(this, ListActivity.class);
-                startActivity(intent);
-                finish();
-            } else {
-                Intent intent = new Intent(this, MainActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        }, DELAY);
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        handler.removeMessages(0);
     }
 
 
